139cbd324cd7d854a95215e677e12c031044cc3d
Jérôme Jossant <jerome.jossant@sourceamax.com>
2022-10-27 22:54:27 +0200

    premier commit

---
+++ app/src/main/java/com/example/locationapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.locationapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore firestore;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+    }
+}
